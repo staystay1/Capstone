4715f2085a5ad8d935de41fa86329c460d5af6ad
Amir Hayes <amir@Amirs-MacBook-Air.local>
2025-04-24 17:18:15 -0400

    api is now working properly

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-const axois = require("axios"); // library to nake https request.
+import axios from "axios"; // library to make https request.
 const apiKey = ""; // api key.
 
-const encodeUrlToBase64 = (url) => {
-  return Buffer.from(url).toString("base64"); // encodes the link into a readable format for the api.
-};
-
 let linkinfo = async (url) => {
-  let encodedUrl = encodeUrlToBase64(url);
-  let endpoint = `https://www.virustotal.com/api/v3/urls/${encodedUrl}`;
-  console.log(endpoint);
+  const encodedUrl = Buffer.from(url).toString("base64").replace(/=+$/, "");
+  const endpoint = `https://www.virustotal.com/api/v3/urls/${encodedUrl}`;
+  console.log("Endpoint:", endpoint);
 
   try {
-    let response = await axois.get(endpoint, {
+    const response = await axios.get(endpoint, {
       headers: {
         "x-apikey": apiKey,
       },
     });
-    console.log(response.data);
+    console.log("Response Data:", response.data);
   } catch (error) {
     console.error(
       "Error obtaining link report:",
       error.response ? error.response.data : error.message
     );
-  } // returns the results and provides error logging.
-}; // returns information about the scanned link.
+  }
+};
 
-linkinfo("https://www.google.com"); // function call, note: replace with a onclick button function later.
+linkinfo("google.com"); // function call, note: replace with a onclick button function later.
